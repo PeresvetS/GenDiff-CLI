33d8885502b050ad0ef4052b5d0a1c880c3c0bd2
PeresvetS <niks280593@gmail.com>
2017-03-25 18:46:28 +0300

    fixed output

--- src/lib/renders/json.js
+++ src/lib/renders/json.js
@@ -1,26 +1,20 @@
-const getDiff = (comparedData) => {
-  const result = comparedData.reduce((acc, elem) => {
-    const { type, key, value } = elem;
-    switch (type) {
-      case 'parent':
-        return { ...acc, [key]: getDiff(value) };
-      case 'changed':
-        return { ...acc, [key]: { added: elem.added, removed: elem.removed } };
-      case 'added':
-        return { ...acc, [key]: { added: value } };
-      case 'removed':
-        return { ...acc, [key]: { removed: value } };
-      case 'unchanged':
-        return { ...acc, [key]: value };
-      default :
-        return { key: 'undefined' };
-        // return !_.has(acc, [type]) ?
-        // { ...acc, type: { [key]: value } } :
-        // _.defaultsDeep(acc, { ...acc, type: { [key]: value } });
-    }
-  }, '');
-  return result;
-};
+const getDiff = comparedData => comparedData.reduce((acc, elem) => {
+  const { type, key, value } = elem;
+  switch (type) {
+    case 'parent':
+      return { ...acc, [key]: getDiff(value) };
+    case 'changed':
+      return { ...acc, [key]: { added: elem.added, removed: elem.removed } };
+    case 'added':
+      return { ...acc, [key]: { added: value } };
+    case 'removed':
+      return { ...acc, [key]: { removed: value } };
+    case 'unchanged':
+      return { ...acc, [key]: value };
+    default :
+      return { key: 'undefined' };
+  }
+}, '');
 
 const toStr = comparedData => JSON.stringify(getDiff(comparedData), '', 2);
 
